dd679453b010888b8d6078991d7856f6a5309ff0
Ernesto Riveiro <ernesto.ernzxr@gmail.com>
2023-11-14 01:11:32 -0300

    Flowbite React
    Se instala Flowbite React con Next en sus ultimas versiones.

--- engine-truck/tailwind.config.js
+++ engine-truck/tailwind.config.js
@@ -1,21 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite/**/*.js",
     "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
     "./public/**/*.html",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [
     require("flowbite/plugin")
