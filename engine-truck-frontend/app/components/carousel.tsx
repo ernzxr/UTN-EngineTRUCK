2de499867282be11aeb490b789ae2fec2ad03c4d
PuppyProgrammer <facuchena@hotmail.com>
2023-11-23 20:03:02 -0300

    finished carousel

--- engine-truck-frontend/app/components/carousel.tsx
+++ engine-truck-frontend/app/components/carousel.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
+
 
 const Carousel = () => {
     const slides = [
@@ -9,9 +12,47 @@ const Carousel = () => {
           url: '/infermon.png'
         }
       ]
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    const prevSlide = () => {
+      const isFirstSlide = currentIndex === 0;
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+      setCurrentIndex(newIndex);
+    }
+
+    const nextSlide = () => {
+      const isLastSlide = currentIndex === slides.length - 1;
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex(newIndex);
+    }
+
+    const goToSlide = (slideIndex: React.SetStateAction<number>) => {
+      setCurrentIndex(slideIndex);
+    }
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        nextSlide();
+      }, 5000);
   
+      return () => clearInterval(intervalId);
+    }, [currentIndex]);
+
+
     return (
-    <div style={{backgroundImage: `url(${slides[0].url})`}}className="h-full w-full rounded-[100px] mx-3 bg-center bg-cover duration-500"></div>
+    <>
+    <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className="h-[90%] w-[95%] m-auto rounded-[100px] border-2 border-black bg-center bg-cover duration-500"></div>
+    <div onClick={prevSlide} className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> <BsChevronCompactLeft size={30}/></div>
+    <div onClick={nextSlide} className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'> <BsChevronCompactRight size={30}/></div>
+    <div className="flex top-4 justify-center py-2">
+      {slides.map((slide, slideIndex) => (
+        // eslint-disable-next-line react/jsx-key
+        <div key={slideIndex} onClick={() => goToSlide(slideIndex)}className="text-2xl cursor-pointer">
+          <RxDotFilled />
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
--- engine-truck-frontend/app/page.tsx
+++ engine-truck-frontend/app/page.tsx
@@ -7,13 +7,10 @@ import Carousel from "./components/carousel";
 
 
 export default function Index(): JSX.Element {
-  
-
-
   return (
     <main className="dark:bg-gray-700">
       <Header />
-      <section className="flex box-border h-[500px] w-[1440px] border-4 border-black mx-auto my-auto">
+      <section className="flex box-border h-[500px] w-[1440px] border-4 border-black mx-auto my-auto mt-[100px]">
         <div className="relative box-border h-[450px] w-[700px] border-4 border-black mx-auto my-auto">
           <p className="absolute top-[50px] left-[110px] text-[70px]">Engine TRUCK</p>
           <p className= "absolute pt-[240px] px-[15px] text-[22px] text-center">
@@ -22,7 +19,7 @@ export default function Index(): JSX.Element {
             nuestro compromiso es brindar soluciones de primera clase
           </p>
         </div>
-        <div className="flex box-border h-[450px] w-[700px] border-4 border-black mx-auto my-auto">
+        <div className="relative group box-border h-[450px] w-[700px] border-4 border-black mx-auto my-auto">
           <Carousel />
         </div>
       </section>
