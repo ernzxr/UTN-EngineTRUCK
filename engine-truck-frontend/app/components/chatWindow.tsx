62b410ca045539ddc52898f1d9f5179aa5e4d078
PuppyProgrammer <facuchena@hotmail.com>
2023-11-30 00:02:12 -0300

    animation on chatReview

--- engine-truck-frontend/app/components/chatWindow.tsx
+++ engine-truck-frontend/app/components/chatWindow.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const ChatWindow = (props: any) => {
-  return (
-    <div className='flex justify-center items-center h-[150px] w-[700px] border-[2px] rounded-[15px] border-black'>{props.review}</div>
-  )
-}
+  const [flipChat, setFlipChat] = useState(false);
 
-export default ChatWindow
+  return (
+    <div
+      onMouseEnter={() => setFlipChat(true)}
+      onMouseLeave={() => setFlipChat(false)}
+    >
+      {flipChat ? (
+        <>
+           <div className="flex justify-center items-center h-[150px] w-[700px] border-[2px] rounded-[15px] border-black dark:border-white dark:text-white animate-jump-in">
+            {props.review}
+          </div>
+        </>
+      ) : (
+        <div className="flex justify-center items-center h-[150px] w-[700px] border-[2px] rounded-[15px] border-black dark:border-white dark:text-white">
+            {props.review}
+          </div>
+      )}
+    </div>
+  );
+};
+
+export default ChatWindow;
--- engine-truck-frontend/app/components/navCard.tsx
+++ engine-truck-frontend/app/components/navCard.tsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 
+
 const NavCard = (props: any) => {
   const [cardHover, setCardHover] = useState(false);
 
   return (
     <div
       style={{ backgroundImage: `url("${props.imagen}")` }}
-      className="flex w-full h-full items-center justify-center shadow-lg bg-cover bg-center  dark:text-white hover:brightness-50 hover:duration-300 cursor-pointer rounded-[10px] "
+      className="flex w-full h-full items-center justify-center shadow-lg bg-cover bg-center dark:text-white hover:brightness-50 hover:duration-300 cursor-pointer rounded-[10px] "
       onMouseEnter={() => setCardHover(true)}
       onMouseLeave={() => setCardHover(false)}
     >
       {cardHover ? (
         <>
           <div className="text-white text-center">
-            <h2 className="text-2xl font-bold mb-2">{props.title}</h2>
+            <h2 className="text-2xl font-bold mb-2" >{props.title}</h2>
             <p>{props.description}</p>
           </div>
         </>
