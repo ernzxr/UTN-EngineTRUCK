fe954067f40fec9efd9a35e41665534f1ee07393
PuppyProgrammer <facuchena@hotmail.com>
2023-12-07 01:36:03 -0300

    fixed table dashboard and engineCard scale

--- engine-truck-frontend/app/admin/dashboard/page.tsx
+++ engine-truck-frontend/app/admin/dashboard/page.tsx
@@ -15,46 +15,27 @@ export default function page() {
 
   return (
     <main className="relative dark:bg-gray-700">
-      <table className="flex flex-col w-[1200px] h-full m-auto mt-[100px] border-2 border-black">
+      <table className="flex flex-col w-[1200px] h-full m-auto mt-[100px] border-2 border-black justify-center text-center">
         <thead>
-          <tr className="flex flex-row gap-8 list-none justify-around border-b-2 border-black my-[10px]">
-            <th>Numero</th>
-            <th>Motor</th>
-            <th>Nombre</th>
-            <th>Potencia</th>
-            <th>Stock</th>
+          <tr className="flex flex-row list-none border-b-2 border-black my-[10px] justify-evenly text-center items-center">
+            <th className="w-[20%]">Numero</th>
+            <th className="w-[20%]">Motor</th>
+            <th className="w-[20%]">Nombre</th>
+            <th className="w-[20%]">Potencia</th>
+            <th className="w-[20%]">Stock</th>
           </tr>
         </thead>
-        <tbody>
-        <tr className="flex flex-row list-none border-b justify-around">
-        <th>1</th>
-        <th>Imagen de Motor</th>
-        <th>MotorRePro</th>
-        <th>230-450</th>
-        <th>45 Disponibles</th>
+        <tbody className="text-center">
+        <tr className="flex flex-row list-none border-b justify-evenly text-center items-center">
+        <th className="w-[20%]">1</th>
+        <th className="w-[20%]">Imagen de Motor</th>
+        <th className="w-[20%]">MotorRePro</th>
+        <th className="w-[20%]">230-450</th>
+        <th className="w-[20%]">45 Disponibles</th>
         </tr>
         </tbody>
       </table>
-      <table className="flex flex-col w-[1200px] h-full m-auto mt-[100px] border-2 border-black">
-        <thead>
-          <tr className="flex flex-row gap-8 list-none justify-around border-b-2 border-black my-[10px]">
-            <th>Numero</th>
-            <th>Motor</th>
-            <th>Nombre</th>
-            <th>Potencia</th>
-            <th>Stock</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr className="flex flex-row gap-8 list-none justify-around border-b">
-        <th>Numero</th>
-        <th>Motor</th>
-        <th>Nombre</th>
-        <th>Potencia</th>
-        <th>Stock</th>
-        </tr>
-        </tbody>
-      </table>
+      
     </main>
   );
 }
--- engine-truck-frontend/app/components/LogoCard.tsx
+++ engine-truck-frontend/app/components/LogoCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LogoCard = () => {
   return (
-    <div className='w-[180px] h-[180px] border-black border-2 rounded-full'></div>
+    <div className='w-[180px] h-[180px] border-black border-2 rounded-full hover:scale-110 duration-300'></div>
   )
 }
 
