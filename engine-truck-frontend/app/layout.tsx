362c8cb7f83bfc65b91676dc3130b96d39989e08
Ernesto Riveiro <ernesto.ernzxr@gmail.com>
2023-12-10 21:53:22 -0300

    Updated dependencies

--- engine-truck-frontend/app/layout.tsx
+++ engine-truck-frontend/app/layout.tsx
@@ -1,16 +1,32 @@
-import FlowbiteContext from "./context/FlowbiteContext";
-import { Providers } from "../lib/redux/provider";
 import "./styles/globals.css";
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+import { Providers } from "../lib/redux/provider";
+import { Flowbite, ThemeModeScript } from "flowbite-react";
+import { type FC, type PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+import { flowbiteTheme } from "./styles/theme";
 
+const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout(props: React.PropsWithChildren) {
+export const metadata: Metadata = {
+  title: "EngineTRUCK",
+  description: "EngineTRUCK"
+}
+
+const RootLayout: FC<PropsWithChildren> = function ({ children }) {
   return (
     <Providers>
       <html lang="es">
-        <body>
-          <FlowbiteContext>{props.children}</FlowbiteContext>
+        <head>
+          <ThemeModeScript />
+        </head>
+        <body className={twMerge("bg-gray-50 dark:bg-gray-900", inter.className)}>
+          <Flowbite theme={{ theme: flowbiteTheme }}>{children}</Flowbite >
         </body>
       </html>
     </Providers>
-  );
+ );
 };
+
+export default RootLayout;
