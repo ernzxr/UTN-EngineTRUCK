48e2f34e61835b327a1c5efc047d1c3e0b95f1bb
Ernesto Riveiro <ernesto.ernzxr@gmail.com>
2023-12-14 22:28:50 -0300

    Merged old commits

--- engine_truck_frontend/app/components/EngineCard.tsx
+++ engine_truck_frontend/app/components/EngineCard.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link';
 const EngineCard = () => {
   return (
     <Link href="/motores/articuloMotor" className="flex flex-col items-center w-[300px] h-[450px] shadow-lg bg-white m-[4%] rounded-[10px] cursor-pointer hover:shadow-2xl hover:shadow-blue-900 duration-300 dark:hover:shadow-2xl dark:hover:shadow-blue-200 hover:scale-105">
-        <div className="flex w-[70%] h-[45%]  border-black border-2 rounded-[10px] mt-16"></div>
-        <div className="mt-16 dark:text-white">NOMBRE DE MOTOR</div> 
+        <div className="flex w-[70%] h-[45%] mt-16">
+          <img src="/prueba_motor_card.jpg" alt="" className='rounded-[10px]'/>
+        </div>
+        <div className="mt-16 dark:text-white">OM 366 Mercedes-Benz</div> 
+        <div className="mt-auto ml-4 px-4 py-2 mb-7 bg-blue-400 text-white rounded-md">Mas Info</div>
     </Link>
   );
 };
--- engine_truck_frontend/app/components/LogoCard.tsx
+++ engine_truck_frontend/app/components/LogoCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LogoCard = (props: any) => {
   return (
     <div className='w-[180px] h-[180px] '>
-      <img src={props.imagen} alt="" className='border-black border-2 rounded-full hover:scale-110 duration-300'/>
+      <img src={props.imagen} alt="" className='shadow-md rounded-full hover:scale-110 duration-300'/>
     </div>
   )
 }
