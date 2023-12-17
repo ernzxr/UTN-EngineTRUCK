13a0243f342cfeee5718f5cbffc74c2b003a6927
PuppyProgrammer <facuchena@hotmail.com>
2023-12-17 12:08:14 -0300

    fixed brand.brand error with find method

--- engine_truck_frontend/app/components/dashboard/engines/EngineCRUD.tsx
+++ engine_truck_frontend/app/components/dashboard/engines/EngineCRUD.tsx
@@ -3,14 +3,20 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { fetchEngines, modifiedEngine, removeEngine } from '@/lib/redux/features/enginesSlice';
+import { fetchBrands } from '@/lib/redux/features/brandsSlice';
+import { fetchManufacturers } from '@/lib/redux/features/manufacturersSlice';
 import { EngineResponse } from '@/lib/services/interfaces/engines';
 import { AppDispatch } from '@/lib/redux/store';
 import EngineCreateModal from './EngineCreateModal';
 import EngineUpdateModal from './EngineUpdateModal';
+import { BrandResponse } from '@/lib/services/interfaces/brands';
+import { ManufacturerResponse } from '@/lib/services/interfaces/manufacturers';
 
 export const EngineCRUD = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { enginesList } = useSelector((state: any) => state.enginesReducers);
+  const { brandsList } = useSelector((state: any) => state.brandsReducers);
+  const { manufacturersList } = useSelector((state: any) => state.manufacturersReducers);
 
   const [engineName, setEngineName] = useState('');
   const [modalStates, setModalStates] = useState<{ [key: number]: boolean }>({});
@@ -34,7 +40,7 @@ export const EngineCRUD = () => {
     dispatch(removeEngine(id));
   }
 
-
+ 
 
   return (
     <>
@@ -48,15 +54,16 @@ export const EngineCRUD = () => {
         </Table.Head>
         <Table.Body className="divide-y">
           {enginesList.map((object: EngineResponse) => (
+            
             <Table.Row key={object.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
               <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
                 {object.model}
               </Table.Cell>
               <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                {object.brand.brand}
+              {brandsList.find((objectBrand: BrandResponse) => objectBrand.id === object.brand_id)?.brand || 'marca no encontrada'}
               </Table.Cell>
               <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                {object.manufacturer.manufacturer}
+              {manufacturersList.find((objectManufacturer: ManufacturerResponse) => objectManufacturer.id === object.manufacturer_id)?.manufacturer || 'fabricante no encontrado'}
               </Table.Cell>
               <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
               <ToggleSwitch checked={object.available} onChange={(checked) => { (checked === 1 ? setSwitchAvailable(true) : setSwitchAvailable(false));
