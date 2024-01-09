2d1dbb269851e8d06d9ed50797eb8d8bae48851d
sali184 <co.salgado94@gmail.com>
2024-01-09 14:59:37 -0300

    Update page.tsx

--- client/app/repuestos/page.tsx
+++ client/app/repuestos/page.tsx
@@ -9,8 +9,8 @@ export default function Page() {
       <main className="relative dark:bg-gray-700">
         <Header />
         <div className="flex flex-col justify-center items-center mt-[40px] font-raleway font-bold">
-        <p className="mb-8 text-6xl">Listado de Repuestos</p>
-        <p className="text-xl">EngineTRUCK no solo vende motores semi armados, sino que brinda toda venta de repuestos y partes esenciales para la colocacion del motor.</p>
+        <p className="text-xl text-center mt-[40px]">En EngineTRUCK no solo vendemos motores semi armados, además brindamos soluciones como repuestos y partes esenciales para la colocacion de su motor.</p>
+        <p className="mb-8 text-6xl mt-[40px]">Listado de Repuestos</p>
         </div>
         <section className="flex flex-wrap justify-start h-full w-[1200px] bg-blue-100 rounded-[1%] shadow-lg m-auto mt-[100px] px-1">
           <ComponentCard />
