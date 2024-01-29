3c226b80d5dc694175b4edcdbac59efdbfae5f1a
sali184 <co.salgado94@gmail.com>
2024-01-29 01:42:15 -0300

    correcciones
    corecciones de espacios y disposicion en marcas que trabajaron con nosotros, web y mobile

--- client/app/components/home/WorkingBrands.tsx
+++ client/app/components/home/WorkingBrands.tsx
@@ -1,16 +1,14 @@
-'use client';
-
 import { FC } from 'react';
 import LogoCard from '../LogoCard';
 
 const WorkingBrands: FC<Record<string, never>> = function () {
   return (
-    <div className="flex justify-around items-center">
-    <LogoCard imagen="/la_serenisima_logo.jpg" />
-    <LogoCard imagen="/puma_logo.jpg" />
-    <LogoCard imagen="/tempranera_logo.jpg" />
-    <LogoCard imagen="/valagro_logo.jpg" />
-  </div>
+    <div className="flex flex-wrap justify-around items-center mb-[50px] mt-[50px] xl:mt-7 xl:mb-20">
+      <LogoCard imagen="/la_serenisima_logo.jpg" className="my-4 sm:my-8" />
+      <LogoCard imagen="/puma_logo.jpg" className="my-4 sm:my-8" />
+      <LogoCard imagen="/tempranera_logo.jpg" className="my-4 sm:my-8" />
+      <LogoCard imagen="/valagro_logo.jpg" className="my-4 sm:my-8" />
+    </div>
   );
 }
 
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -15,11 +15,11 @@ export default function Index(): JSX.Element {
         <section>
           <Carousel />
         </section>
-        <section className="w-[80%] h-max bg-black m-auto mt-10">
+        <section className="w-[80%] h-max  m-auto mt-10">
           <TripleLayout />
         </section>
         <section>
-          <h1 className="text-center text-4xl font-bold">EMPRESAS QUE CONFIAN EN NOSOTROS</h1>
+          <h1 className="text-center sm:mt-[10%] sm:mb[10%] xl:mt-[5%] xl:mb-[5%] text-4xl font-bold">EMPRESAS QUE CONFIAN EN NOSOTROS</h1>
           <WorkingBrands />
         </section>
         <section>
