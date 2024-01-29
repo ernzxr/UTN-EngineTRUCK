31880d7a82947876f316bdf498e104317883291a
sali184 <co.salgado94@gmail.com>
2024-01-29 01:30:17 -0300

    links_cards

--- client/app/components/home/TripleLayout.tsx
+++ client/app/components/home/TripleLayout.tsx
@@ -5,11 +5,13 @@ const TripleLayout: FC<Record<string, never>> = function () {
     return (
         <div className="flex flex-wrap justify-center xl:justify-between xl:mx-[15%] sm:justify-start mb-[20%] mt-[20%] xl:mt-[10%] xl:mb-[10%]">
             <Card
-                className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] m-2 text-center "
+                className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] m-2 text-center shadow-sm "
                 imgAlt="Meaningful alt text for an image that is not purely decorative"
                 imgSrc="MOT_CARD.jpg"
+                href={"/motores"}
+
             >
-                <h5 className="text-2xl font-raleway font-bold centerw tracking-tight shadow-sm text-gray-900 dark:text-white ">
+                <h5 className="text-2xl font-raleway font-bold centerw tracking-tight text-gray-900 dark:text-white ">
                     MOTORES
                 </h5>
             </Card>
@@ -17,6 +19,7 @@ const TripleLayout: FC<Record<string, never>> = function () {
                 className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] sm:mt-3 m-2 text-center shadow-sm"
                 imgAlt="Meaningful alt text for an image that is not purely decorative"
                 imgSrc="REP_CARD.jpg"
+                href={"/repuestos"}
             >
                 <h5 className="text-2xl font-raleway font-bold tracking-tight text-gray-900 dark:text-white">
                     REPUESTOS
@@ -26,6 +29,7 @@ const TripleLayout: FC<Record<string, never>> = function () {
                 className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] sm:mt-3 m-2 text-center shadow-sm"
                 imgAlt="Meaningful alt text for an image that is not purely decorative"
                 imgSrc="CONT_CARD.jpg"
+                href={"/contacto"}
             >
                 <h5 className="text-2xl font-raleway font-bold tracking-tight text-gray-900 dark:text-white">
                     CONTACTO
