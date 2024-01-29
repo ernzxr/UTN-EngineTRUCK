6aab51b9d000baa353df3921ec01c4dcb5118d40
sali184 <co.salgado94@gmail.com>
2024-01-29 02:28:10 -0300

    correccion de imagenes carousel

--- client/app/components/home/Carousel.tsx
+++ client/app/components/home/Carousel.tsx
@@ -6,11 +6,11 @@ const Carousel = () => {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <CarouselReact pauseOnHover>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-        <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <img src="img_c1.jpg" alt="img_c1.jpg" />
+        <img src="img_c2.jpg" alt="img_c2.jpg" />
+        <img src="img_c3.jpg" alt="img_c3.jpg" />
+        <img src="img_c4.jpg" alt="img_c4.jpg" />
+        <img src="img_c5.jpg" alt="img_c5.jpg" />
       </CarouselReact>
     </div>
   );
