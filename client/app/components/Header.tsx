c5066bb96c25700159db2356d7b03dfa90a1dce7
sali184 <co.salgado94@gmail.com>
2024-01-29 13:29:54 -0300

    correcio_header
    logre que el menu se adapte tanto a la version web como a la mobile. ahora repliega y logre acomodar tipografia y colores, los links estan funcionando bien

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -8,32 +8,45 @@ const Header: FC<Record<string, never>> = function () {
   return (
     <header className="sticky top-0 z-20 border-b-[1px] border-gray-700 dark:border-blue-700 ">
       <Navbar fluid className="bg-blue-950 ">
-        <Navbar.Brand href="/">
-          <Image className="ml-[20%] mr-[15%]"
+        <Navbar.Brand href="/" className="flex items-center">
+          <Image
             alt="Flowbite logo"
             height="24"
             src="/engine_truck_ovallogo.png"
             width="54"
-            
+            className="mx-auto md:ml-[20%] md:mr-[15%] mb-2 md:mb-0"
           />
-          <span className="self-center whitespace-nowrap px-3 text-xl font-semibold text-white ml-3">
+          <span className="self-center whitespace-nowrap text-xl font-semibold text-white ml-3">
             EngineTRUCK
           </span>
         </Navbar.Brand>
-        <div className="flex md:order-2 ">
+        <div className="flex md:order-2">
           <Navbar.Toggle />
           <DarkThemeToggle />
         </div>
-        <Navbar.Collapse>
-          <div className="flex space-x-20 font-raleway font-bold  ">
-          <Navbar.Link className="hover:border-white hover:border-b-[1px] hover:dark:border-white  text-white dark:text-white " as={Link} href="/" >INICIO</Navbar.Link>
-          <Navbar.Link className="hover:border-white hover:border-b-[1px] hover:dark:border-white  text-white dark:text-white " as={Link} href="/motores">MOTORES</Navbar.Link>
-          <Navbar.Link className="hover:border-white hover:border-b-[1px] hover:dark:border-white  text-white dark:text-white " as={Link} href="/repuestos">REPUESTOS</Navbar.Link>
-          <Navbar.Link className="hover:border-white hover:border-b-[1px] hover:dark:border-white  text-white dark:text-white " as={Link} href="/nosotros" >NOSOTROS</Navbar.Link>
-          <Navbar.Link className="hover:border-white hover:border-b-[1px] hover:dark:border-white  text-white dark:text-white " as={Link} href="/contacto">CONTACTO</Navbar.Link>
+        <Navbar.Collapse className="md:hidden">
+          <div className="flex flex-col items-center space-y-4 mt-4">
+            <Navbar.Link as={Link} href="/" className="menu-link font-raleway font-bold text-white ">INICIO</Navbar.Link>
+            <Navbar.Link as={Link} href="/motores" className="menu-link font-raleway font-bold text-white">MOTORES</Navbar.Link>
+            <Navbar.Link as={Link} href="/repuestos" className="menu-link font-raleway font-bold text-white">REPUESTOS</Navbar.Link>
+            <Navbar.Link as={Link} href="/nosotros" className="menu-link font-raleway font-bold text-white">NOSOTROS</Navbar.Link>
+            <Navbar.Link as={Link} href="/contacto" className="menu-link font-raleway font-bold text-white">CONTACTO</Navbar.Link>
           </div>
         </Navbar.Collapse>
+        <Navbar.Collapse className="hidden md:flex space-x-20 ">
+          <Navbar.Link as={Link} href="/" className="menu-link font-raleway font-bold text-white">INICIO</Navbar.Link>
+          <Navbar.Link as={Link} href="/motores" className="menu-link font-raleway font-bold text-white">MOTORES</Navbar.Link>
+          <Navbar.Link as={Link} href="/repuestos" className="menu-link font-raleway font-bold text-white">REPUESTOS</Navbar.Link>
+          <Navbar.Link as={Link} href="/nosotros" className="menu-link font-raleway font-bold text-white">NOSOTROS</Navbar.Link>
+          <Navbar.Link as={Link} href="/contacto" className="menu-link font-raleway font-bold text-white">CONTACTO</Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
+
+      <style jsx>{`
+        .menu-link {
+          color: white !important; /* Cambia el color de texto en modo normal */
+        }
+      `}</style>
     </header>
   );
 };
