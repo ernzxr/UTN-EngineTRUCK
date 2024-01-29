a217f6c166e6255046836db479182e64d8ae3033
sali184 <co.salgado94@gmail.com>
2024-01-29 01:24:59 -0300

    cards_config_and_imgs

--- client/app/components/home/TripleLayout.tsx
+++ client/app/components/home/TripleLayout.tsx
@@ -1,21 +1,37 @@
-'use client'
 import { FC } from "react";
 import { Card } from "flowbite-react";
 
 const TripleLayout: FC<Record<string, never>> = function () {
     return (
-        <>
+        <div className="flex flex-wrap justify-center xl:justify-between xl:mx-[15%] sm:justify-start mb-[20%] mt-[20%] xl:mt-[10%] xl:mb-[10%]">
             <Card
-                className="w-[10px] xl:w-[200px] sm:w-[50px]"
+                className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] m-2 text-center "
                 imgAlt="Meaningful alt text for an image that is not purely decorative"
-                imgSrc="/images/blog/image-1.jpg"
+                imgSrc="MOT_CARD.jpg"
             >
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="text-2xl font-raleway font-bold centerw tracking-tight shadow-sm text-gray-900 dark:text-white ">
                     MOTORES
                 </h5>
             </Card>
-           
-        </>
+            <Card
+                className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] sm:mt-3 m-2 text-center shadow-sm"
+                imgAlt="Meaningful alt text for an image that is not purely decorative"
+                imgSrc="REP_CARD.jpg"
+            >
+                <h5 className="text-2xl font-raleway font-bold tracking-tight text-gray-900 dark:text-white">
+                    REPUESTOS
+                </h5>
+            </Card>
+            <Card
+                className="w-full sm:w-[45%] md:w-[30%] lg:w-[25%] xl:w-[300px] sm:mt-3 m-2 text-center shadow-sm"
+                imgAlt="Meaningful alt text for an image that is not purely decorative"
+                imgSrc="CONT_CARD.jpg"
+            >
+                <h5 className="text-2xl font-raleway font-bold tracking-tight text-gray-900 dark:text-white">
+                    CONTACTO
+                </h5>
+            </Card>
+        </div>
     );
 };
 
