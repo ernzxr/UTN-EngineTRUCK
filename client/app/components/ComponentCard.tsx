80008f4026defa581c4392c1094fdddebf0ecd9a
sali184 <co.salgado94@gmail.com>
2024-01-30 14:40:00 -0300

    correccion/repuestos/navcard

--- client/app/components/ComponentCard.tsx
+++ client/app/components/ComponentCard.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const ComponentCard = () => {
   return (
-    <Link href="/repuestos/articuloRepuesto" className="flex flex-col items-center w-[300px] h-[300px] shadow-lg bg-white m-[4%] rounded-[10px] cursor-pointer hover:shadow-2xl hover:shadow-blue-900 duration-300 dark:hover:shadow-2xl dark:hover:shadow-blue-200 hover:scale-105">
-        <div className="flex w-[70%] h-[60%]  border-black border-2 rounded-[10px] mt-12"></div>
-        <div className="mt-6 dark:text-white">NOMBRE DE REPUESTO</div> 
+    <Link href="/repuestos/articuloRepuesto" className="flex flex-col items-center xl:w-[20%] w-[80%] h-[300px] shadow-lg bg-white m-[4%] rounded-[10px] cursor-pointer hover:shadow-2xl hover:shadow-blue-900 duration-300 dark:hover:shadow-2xl dark:hover:shadow-blue-200 hover:scale-105">
+        <div className="flex w-[70%] h-[60%]  border-black border-2 rounded-[10px] mt-[14%]"></div>
+        <div className="mt-[5%] dark:text-white">NOMBRE DE REPUESTO</div> 
     </Link>
   )
 }
--- client/app/repuestos/page.tsx
+++ client/app/repuestos/page.tsx
@@ -8,17 +8,18 @@ export default function Page() {
     return (
       <main className="relative dark:bg-gray-700">
         <Header />
-        <img src="/cam_t1.jpg" alt="" className="w-full h-[450px]"/>
-        <section className="relative h-full w-[1200px] bg-blue-100 rounded-[5px] shadow-lg m-auto mt-[10px] p-[75px] bottom-10">
-        <p className="text-5xl font-raleway text-center font-bold text-blue-900">NUESTRO LISTADO DE REPUESTOS</p>
-        <p className="text-xl mt-[40px] font-raleway text-center font-bold">En EngineTRUCK no solo vendemos motores semi armados, además brindamos soluciones como repuestos y partes esenciales para la colocacion de su motor</p>
+        <img src="/cam_t1.jpg" alt="" className="w-full h-[80%]"/>
+        <section className="relative h-full  w-[80%] xl:w-[70%] bg-blue-100 rounded-[5px] shadow-lg m-auto mt-[1%] p-[7%] bottom-4 xl:bottom-10">
+        <p className="xl:text-6xl text-xl font-raleway text-center font-bold text-blue-900">NUESTRO LISTADO DE REPUESTOS</p>
+        <p className="xl:text-xl text-m mt-[2%] xl:mt-[5%] font-raleway text-center font-bold">En EngineTRUCK no solo vendemos motores semi armados, además brindamos soluciones como repuestos y partes esenciales para la colocacion de su motor</p>
         </section>
-        <section className="flex flex-wrap justify-start h-full w-[1200px] bg-blue-100 rounded-[1%] shadow-lg m-auto mt-[100px] px-1">
+        <section className="flex flex-wrap justify-center items-center h-full w-[90%] xl:w-[80%] bg-blue-100 rounded-[5px] shadow-lg m-auto mt-[10%] px-1">
           <ComponentCard />
           <ComponentCard />
           <ComponentCard />
           <ComponentCard />
         </section>
+
         <footer className="w-full ">
           <Footer />
         </footer>
