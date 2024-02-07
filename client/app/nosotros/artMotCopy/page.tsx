46e1a5c9c41665f6a12156596e14d0c7c501c8b0
sali184 <co.salgado94@gmail.com>
2024-02-07 14:41:38 -0300

    Update page.tsx
    falta terminar de modificar

--- client/app/nosotros/artMotCopy/page.tsx
+++ client/app/nosotros/artMotCopy/page.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { Card } from 'flowbite-react';
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/FooterReact";
 import { EngineResponse } from "@/lib/services/interfaces/engines";
@@ -24,39 +25,24 @@ export default function Page({params}: any) {
   const motorSeleccionado = enginesList.find((motor: EngineResponse) => motor.id === model);  
 
  
-    return (
+    return (///mt-[10px]///
       <>
       <main className="relative dark:bg-gray-700">
       <Header />
-      <section className="flex w-[1200px] h-[600px] m-auto mt-[10px] bg-blue-100 rounded-[30px] shadow-lg">
-        <img
-          src="/mot_t2.jpg"
-          alt="Motor"
-          className="w-[50%] h-[450px] m-10 rounded-[20px] shadow-md"
-        />
-        <div className="flex flex-col justify-evenly w-[45%] h-[75%] m-10">
-          <p className="text-4xl font-raleway font-bold">
-            
-          </p>
-          <p className="text-2xl font-raleway">
-            Principales Caracteristicas:
-          </p>
-          <table>
-            <tbody>
-              <tr className="flex flex-col text-xl font-raleway">
-                <td className="p-[10px]"></td>
-                <td className="p-[10px]"></td>
-                <td className="p-[10px]"></td>
-                <td className="p-[10px]"></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="absolute top-[580px] left-[20%] m-auto w-[58%] border-blue-300 border-2"></div>
-        <button className="absolute top-[600px] left-[20%] mt-auto ml-4 px-4 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+      <section className="flex xl:w-[70%] xl:h-[600px] sm:h-[100%] sm:w[100%] m-auto mt-10 mb-10  bg-blue-100 rounded-[30px] shadow-lg">
+      <Card className="max-w-sm" imgSrc="MOT_CARD.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        Noteworthy technology acquisitions 2021
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+        <div className="absolute top-[80%] left-[20%] m-auto w-[58%] border-blue-300 border-2"></div>
+        <button className="absolute top-[74%] left-[20%] mt-auto ml-4 px-4 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
           Volver
         </button>
-        <button className="absolute top-[600px] left-[65%] mt-auto ml-4 px-12 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+        <button className="absolute top-[74%] left-[65%] mt-auto ml-4 px-12 py-2 bg-blue-800 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
           Consultar
         </button>
       </section>
